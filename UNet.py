ab9d3e0d9055f11cf3d7e94dba7a79ab55b72454
andibraimllari <andibraimllari@gmail.com>
2021-05-30 14:52:24 +0200

    initial UNet architecture

--- UNet.py
+++ UNet.py
@@ -1,5 +1,105 @@
 import torch.nn as nn
+import torch.nn.functional as F
 
 
+# TODO pooling layers do not have parameters, double check
+# NB there are 4 crops
 class UNet(nn.Module):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 64, (3, 3))
+        self.conv2 = nn.Conv2d(64, 64, (3, 3))
+
+        self.pool1 = nn.MaxPool2d(2, 2)
+
+        self.conv3 = nn.Conv2d(64, 128, (3, 3))
+        self.conv4 = nn.Conv2d(128, 128, (3, 3))
+
+        self.pool2 = nn.MaxPool2d(2, 2)
+
+        self.conv5 = nn.Conv2d(128, 256, (3, 3))
+        self.conv6 = nn.Conv2d(256, 256, (3, 3))
+
+        self.pool3 = nn.MaxPool2d(2, 2)
+
+        self.conv7 = nn.Conv2d(256, 512, (3, 3))
+        self.conv8 = nn.Conv2d(512, 512, (3, 3))
+
+        self.pool4 = nn.MaxPool2d(2, 2)
+
+        self.conv9 = nn.Conv2d(512, 1024, (3, 3))
+        self.conv10 = nn.Conv2d(1024, 1024, (3, 3))
+
+        self.transpConv1 = nn.ConvTranspose2d(1024, 512, (2, 2))
+
+        self.conv11 = nn.Conv2d(1024, 512, (3, 3))
+        self.conv12 = nn.Conv2d(512, 512, (3, 3))
+
+        self.transpConv2 = nn.ConvTranspose2d(512, 256, (2, 2))
+
+        self.conv13 = nn.Conv2d(512, 256, (3, 3))
+        self.conv14 = nn.Conv2d(256, 128, (3, 3))
+
+        self.transpConv3 = nn.ConvTranspose2d(256, 128, (2, 2))
+
+        self.conv15 = nn.Conv2d(256, 128, (3, 3))
+        self.conv16 = nn.Conv2d(128, 128, (3, 3))
+
+        self.transpConv4 = nn.ConvTranspose2d(128, 64, (2, 2))
+
+        self.conv17 = nn.Conv2d(128, 64, (3, 3))
+        self.conv18 = nn.Conv2d(64, 64, (3, 3))
+
+        self.conv1x1 = nn.Conv2d(64, 2, (1, 1))  # TODO does this need to be 2, or can it be 1? what do we need
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        # TODO first crop here
+
+        x = self.pool1(x)
+
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        # TODO second crop here
+
+        x = self.pool2(x)
+
+        x = F.relu(self.conv5(x))
+        x = F.relu(self.conv6(x))
+        # TODO third crop here
+
+        x = self.pool3(x)
+
+        x = F.relu(self.conv7(x))
+        x = F.relu(self.conv8(x))
+        # TODO fourth crop here
+
+        x = self.pool4(x)
+
+        x = F.relu(self.conv9(x))
+        x = F.relu(self.conv10(x))
+
+        x = self.transpConv1(x)
+
+        x = F.relu(self.conv11(x))  # TODO x conc fourth crop
+        x = F.relu(self.conv12(x))
+
+        x = self.transpConv2(x)
+
+        x = F.relu(self.conv13(x))  # TODO x conc third crop
+        x = F.relu(self.conv14(x))
+
+        x = self.transpConv3(x)
+
+        x = F.relu(self.conv15(x))  # TODO x conc second crop
+        x = F.relu(self.conv16(x))
+
+        x = self.transpConv4(x)
+
+        x = F.relu(self.conv17(x))  # TODO x conc first crop
+        x = F.relu(self.conv18(x))
+
+        x = self.conv1x1(x)
+
+        return x
---
+++ unet_playground.py
@@ -0,0 +1,151 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+from matplotlib import pyplot as plt
+from skimage import color
+from skimage import io
+from ttictoc import tic, toc
+from scipy import misc
+from UNet import UNet
+
+
+# ========== definitions start here ==========
+def imshow(img):
+    img = img / 2 + 0.5  # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+# ========== playground start here ==========
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+batch_size = 4
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                          shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                         shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+# get some random training images
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+
+# # show images
+# imshow(torchvision.utils.make_grid(images))
+# # print labels
+# print(' '.join('%5s' % classes[labels[j]] for j in range(batch_size)))
+
+unet = UNet()
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(unet.parameters(), lr=0.001, momentum=0.9)
+
+for epoch in range(2):  # loop over the dataset multiple times
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = unet(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:  # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+print('Finished Training')
+
+# PATH = './cifar_net.pth'
+# torch.save(unet.state_dict(), PATH)
+#
+# dataiter = iter(testloader)
+# images, labels = dataiter.next()
+#
+# # print images
+# imshow(torchvision.utils.make_grid(images))
+# print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+#
+# net = UNet()
+# net.load_state_dict(torch.load(PATH))
+#
+# outputs = net(images)
+#
+# _, predicted = torch.max(outputs, 1)
+#
+# print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(4)))
+
+# ========== PyTorch demo code ==========
+
+# model = torch.hub.load('pytorch/vision:v0.9.0', 'fcn_resnet101', pretrained=True)
+# # print(model.eval(color.rgb2gray(io.imread('slice_511.jpg'))))
+#
+# # Download an example image from the pytorch website
+# import urllib
+#
+# url, filename = ("https://github.com/pytorch/hub/raw/master/images/dog.jpg", "dog.jpg")
+# try:
+#     urllib.URLopener().retrieve(url, filename)
+# except:
+#     urllib.request.urlretrieve(url, filename)
+#
+# filename = 'slice_511.jpg'
+#
+# # sample execution (requires torchvision)
+# from PIL import Image
+# from torchvision import transforms
+#
+# input_image = Image.open(filename)
+# preprocess = transforms.Compose([
+#     transforms.ToTensor(),
+#     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+# ])
+#
+# input_tensor = preprocess(input_image)
+# input_batch = input_tensor.unsqueeze(0)  # create a mini-batch as expected by the model
+#
+# # move the input and model to GPU for speed if available
+# if torch.cuda.is_available():
+#     print('here boi')
+#     input_batch = input_batch.to('cuda')
+#     model.to('cuda')
+#
+# with torch.no_grad():
+#     output = model(input_batch)['out'][0]
+# output_predictions = output.argmax(0)
+#
+# # create a color pallette, selecting a color for each class
+# palette = torch.tensor([2 ** 25 - 1, 2 ** 15 - 1, 2 ** 21 - 1])
+# colors = torch.as_tensor([i for i in range(21)])[:, None] * palette
+# colors = (colors % 255).numpy().astype("uint8")
+#
+# # plot the semantic segmentation predictions of 21 classes in each color
+# r = Image.fromarray(output_predictions.byte().cpu().numpy()).resize(input_image.size)
+# r.putpalette(colors)
+#
+# import matplotlib.pyplot as plt
+# plt.imshow(r)
+# plt.show()
