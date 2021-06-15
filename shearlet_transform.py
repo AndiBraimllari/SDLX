25329c905555cb2434979ce0d6ee834f5d26f082
andibraimllari <andibraimllari@gmail.com>
2021-06-15 23:45:02 +0200

    minor refactoring and rewording

--- shearlet_transform.py
+++ shearlet_transform.py
@@ -64,23 +64,10 @@ def psiHat2(w):  # R -> R
 
 
 def psiHat(w1, w2):  # separable generating function?
-    # TODO how is this function defined on w1 := 0?
+    # TODO how is this function defined on w1 := 0? maybe psiHat1(w1) * psiHat2(w2) ? if psiHat1(w1) returns 0 then ???
     if w1 == 0:
         return 0
     return psiHat1(w1) * psiHat2(w2 / w1)
-
-
-def psiHathxv(w1, w2):  # separable generating function?
-    # TODO add checks over the input here?
-    res = 0
-    if abs(w1) >= 1 / 2 and abs(w2) < abs(w1):
-        res += psiHat(w1, w2)
-    if abs(w2) >= 1 / 2 and abs(w2) > abs(w1):
-        res += psiHat(w2, w1)
-    if abs(w1) >= 1 / 2 and abs(w2) >= 1 / 2 and abs(w2) == abs(w1):
-        res += psiHat(w1, w2)
-    return res
-    # return psiHat(w1, w2) + psiHat(w2, w1) + psiHat(w1, w2)
 
 
 # ===== shearlet transforms starts here =====
@@ -90,52 +77,41 @@ def applyShearletTransform(img):
     N = img.shape[1]
 
     # currently the values of the image vary from [0, 1]
-    # img /= M # should I rescale the image? might be required for the low-frequency part
+    # img /= M # TODO should I rescale the image? might be required for the low-frequency part
 
     jZero = np.floor(1 / 2 * np.log2(max(M, N))).astype('int')
     eta = 2 ** (jZero + 2) - 3
 
-    # NB numpy isn't well suited for varying size arrays, so I'm using Python lists
-    # on a second note, the output always seems to be 61 x 511 x 511, confirm before changing
     SHf = np.zeros([eta, M, N], dtype=complex)
 
     fftImg = np.fft.fft2(img)
     print('Shape of the input image in the shearlet transform is:', fftImg.shape)
 
-    # NB sampling from is [-255, 255] x [-255, 255]
-
+    # sampling from is [-255, 255] x [-255, 255]
     i = 0
-    # TODO memoize spectra?
-    # [0, ..., j0 - 1]
     for j in range(jZero):
-        # [-2^j, ..., 2^j]
         for k in range(-2 ** j, 2 ** j + 1):
             tempSHSectionh = np.zeros([M, N], dtype=complex)
             tempSHSectionv = np.zeros([M, N], dtype=complex)
             tempSHSectionhxv = np.zeros([M, N], dtype=complex)
-            CURR_TEMP = np.zeros([M, N], dtype=complex)
             # [-floor(M / 2), ..., ceil(M / 2) - 1]
             for w1 in range(-np.floor(M / 2).astype('int'), np.ceil(M / 2).astype('int')):
                 # [-floor(N / 2), ..., ceil(N / 2) - 1]
                 for w2 in range(-np.floor(N / 2).astype('int'), np.ceil(N / 2).astype('int')):
+                    horiz = 0
+                    vertic = 0
+                    if abs(w2) <= abs(w1):
+                        horiz = psiHat(4 ** (-j) * w1, 4 ** (-j) * k * w1 + 2 ** (-j) * w2)
+                    else:
+                        vertic = psiHat(4 ** (-j) * w2, 4 ** (-j) * k * w2 + 2 ** (-j) * w1)
                     if abs(k) <= 2 ** j - 1:
                         # section of horizontal cone
-                        tempSHSectionh[w1, w2] = psiHat(4 ** (-j) * w1, 4 ** (-j) * k * w1 + 2 ** (-j) * w2) * \
-                                                 fftImg[w1][w2]
+                        tempSHSectionh[w1, w2] = horiz * fftImg[w1][w2]
                         # section of vertical cone
-                        tempSHSectionv[w1, w2] = psiHat(4 ** (-j) * w2, 4 ** (-j) * k * w2 + 2 ** (-j) * w1) * \
-                                                 fftImg[w1][w2]
+                        tempSHSectionv[w1, w2] = vertic * fftImg[w1][w2]
                     elif abs(k) == 2 ** j:
-                        # TODO observe values of w1 and w2
-                        # print(w1, w2)
                         # section of the seam lines
-                        if abs(w2) <= abs(w1):
-                            tempSHSectionhxv[w1, w2] = psiHat(4 ** (-j) * w1, 4 ** (-j) * k * w1 + 2 ** (-j) * w2) * \
-                                                       fftImg[w1][w2]
-                        else:
-                            tempSHSectionhxv[w1, w2] = psiHat(4 ** (-j) * w2, 4 ** (-j) * k * w2 + 2 ** (-j) * w1) * \
-                                                       fftImg[w1][w2]
-                        CURR_TEMP[w1, w2] = psiHathxv(4 ** (-j) * w1, 4 ** (-j) * k * w1 + 2 ** (-j) * w2)
+                        tempSHSectionhxv[w1, w2] = (horiz + vertic) * fftImg[w1][w2]
             if abs(k) <= 2 ** j - 1:
                 SHSectionh = np.fft.ifft2(tempSHSectionh)
                 if SHSectionh.all() != np.real(SHSectionh).all():
@@ -154,10 +130,8 @@ def applyShearletTransform(img):
                     raise ValueError('Unexpected state! Non-zero imaginary parts found in SHSectionhxv!')
                 SHf[i] = SHSectionhxv
                 i += 1
-                plt.imshow(np.real(CURR_TEMP), cmap='gray')  # cmap='gray'  # np.imag
-                plt.show()
-                plt.imshow(np.real(SHSectionhxv), cmap='gray')  # cmap='gray'  # np.imag
-                plt.show()
+                # plt.imshow(np.real(SHSectionhxv), cmap='gray')  # cmap='gray'  # np.imag
+                # plt.show()
 
     # TODO current problem, phiHat returns non-zero for input around [-1, 1] but our w1 and w2 vary from [-255, 255]
     #  is it okay that most of the low-frequency output is 0? or should I scale w1 an w2?
@@ -178,39 +152,37 @@ def applyShearletTransform(img):
 
 def applyInverseShearletTransform(SHf):
     tic()
-    # NB here I'm assuming SHf is of shape (eta, M, N)
+    # here I'm assuming SHf is of shape (eta, M, N)
     M = SHf.shape[1]
     N = SHf.shape[2]
     jZero = np.floor(1 / 2 * np.log2(max(M, N))).astype('int')
 
     fHat = np.zeros([M, N], dtype=complex)
 
-    # NB sampling from is [-255, 255] x [-255, 255]
-
+    # sampling from is [-255, 255] x [-255, 255]
     i = 0
-
-    # [0, ..., j0 - 1]
     for j in range(jZero):
-        # [-2^j, ..., 2^j]
         for k in range(-2 ** j, 2 ** j + 1):
-            tempSHSectionh = np.zeros([M, N], dtype=complex)
-            tempSHSectionv = np.zeros([M, N], dtype=complex)
-            tempSHSectionhxv = np.zeros([M, N], dtype=complex)
-            # [-floor(M / 2), ..., ceil(M / 2) - 1]
+            tempSHSectionh = np.zeros([M, N])
+            tempSHSectionv = np.zeros([M, N])
+            tempSHSectionhxv = np.zeros([M, N])
             for w1 in range(-np.floor(M / 2).astype('int'), np.ceil(M / 2).astype('int')):
                 # [-floor(N / 2), ..., ceil(N / 2) - 1]
                 for w2 in range(-np.floor(N / 2).astype('int'), np.ceil(N / 2).astype('int')):
+                    horiz = 0
+                    vertic = 0
+                    if abs(w2) <= abs(w1):
+                        horiz = psiHat(4 ** (-j) * w1, 4 ** (-j) * k * w1 + 2 ** (-j) * w2)
+                    else:
+                        vertic = psiHat(4 ** (-j) * w2, 4 ** (-j) * k * w2 + 2 ** (-j) * w1)
                     if abs(k) <= 2 ** j - 1:
                         # section of horizontal cone
-                        tempSHSectionh[w1, w2] = psiHat(4 ** (-j) * w1, 4 ** (-j) * k * w1 + 2 ** (-j) * w2)
+                        tempSHSectionh[w1, w2] = horiz
                         # section of vertical cone
-                        tempSHSectionv[w1, w2] = psiHat(4 ** (-j) * w2, 4 ** (-j) * k * w2 + 2 ** (-j) * w1)
+                        tempSHSectionv[w1, w2] = vertic
                     elif abs(k) == 2 ** j:
                         # section of the seam lines
-                        if abs(w2) <= abs(w1):
-                            tempSHSectionhxv[w1, w2] = psiHat(4 ** (-j) * w1, 4 ** (-j) * k * w1 + 2 ** (-j) * w2)
-                        else:
-                            tempSHSectionhxv[w1, w2] = psiHat(4 ** (-j) * w2, 4 ** (-j) * k * w2 + 2 ** (-j) * w1)
+                        tempSHSectionhxv[w1, w2] = horiz + vertic
             if abs(k) <= 2 ** j - 1:
                 SHtSectionh = np.multiply(np.fft.fft2(SHf[i]), tempSHSectionh)
                 i += 1
@@ -226,7 +198,7 @@ def applyInverseShearletTransform(SHf):
             #     print(?)
 
     # section of the low frequency
-    tempSHtcSectionZero = np.zeros([M, N], dtype=complex)
+    tempSHtcSectionZero = np.zeros([M, N])
 
     for w1 in range(-np.floor(M / 2).astype('int'), np.ceil(M / 2).astype('int')):
         for w2 in range(-np.floor(N / 2).astype('int'), np.ceil(N / 2).astype('int')):
@@ -255,67 +227,3 @@ def shearlet_demo(imagePath):
 
 
 shearlet_demo('slice_511.jpg')
-
-# NB use this to avoid running a transform
-# import pickle
-# with open('shearletCoeffs.obj', 'wb') as fp:
-#     pickle.dump(shearletCoeffs, fp)
-# with open('shearletCoeffs.obj', 'rb') as fp:
-#     shearletCoeffs = pickle.load(fp)
-
-
-# ========== deprecated stuff ==========
-
-# import sys
-# np.set_printoptions(threshold=sys.maxsize)
-# np.savetxt("arr.txt", potRec, newline="\n")
-#
-# if np.real(reconstruction.all()) != reconstruction.all():
-#     raise ValueError('Shouldn\'t they all be real values?')
-
-# NB torch can also be used:
-# npFft = np.fft.fft([0, 1, 2, 3, 4])
-# print(npFft)
-# torchFft = torch.fft.fft2('grs_512.jpg')
-# torchFft = torch.fft.fft(torch.IntTensor([0, 1, 2, 3, 4]))
-# print(torchFft)
-
-# potential plan:
-# fft2
-# fftshift
-# math here
-# ifftshift
-# ifft2
-
-# # NB this is a quick indexing generator of [-255, 255] x [-255, 255]
-# for i in range(-M // 2 + 1, M // 2 + 1):
-#     for j in range(-N // 2 + 1, N // 2 + 1):
-
-# # NB parabolic scaling matrix and shearing matrix, currently infused in the methods
-# def Aa(aParam):  # aParam has to be positive
-#     return np.array([
-#         [aParam, 0],
-#         [0, np.sqrt(aParam)]
-#     ])
-# def Ss(sParam):
-#     return np.array([
-#         [1, sParam],
-#         [0, 1]
-#     ])
-
-# # NB negative indexing in Python may replace the need for this
-# periodicImage = np.zeros([2 * M, 2 * N])
-# for i in range(-M, M):
-#     for j in range(-N, N):
-#         # Q2
-#         if i < 0 and j > 0:
-#             periodicImage[i, j] = img[i + M, j]
-#         # Q3
-#         elif i < 0 and j < 0:
-#             periodicImage[i, j] = img[i + M, j + N]
-#         # Q4
-#         elif i > 0 and j < 0:
-#             periodicImage[i, j] = img[i, j + N]
-#         # Q1
-#         else:
-#             periodicImage[i, j] = img[i, j]
