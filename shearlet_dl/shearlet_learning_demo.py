5cb33076545fdd804d47eb7612d85153bdd7bd0d
andibraimllari <andibraimllari@gmail.com>
2021-08-10 20:16:50 +0200

    write a more detailed plan for tackling limited angle/sparse CT

--- shearlet_dl/shearlet_learning_demo.py
+++ shearlet_dl/shearlet_learning_demo.py
@@ -1,40 +1,102 @@
-import torch
 from matplotlib import pyplot as plt
-from skimage import color
-from skimage import io
 
 from shearlet_dl.PhantomNet import PhantomNet
-from shearlet_transform.shearlet_transform_algorithm import applyShearletTransform as SH
+from shearlet_transform.shearlet_transform_algorithm import applyShearletTransform, applyInverseShearletTransform
 
 
-def shearlet_learning_demo():
+def generate_limited_angle_ct_data(dir):
+    for i in range(len(data)):
+        # fetch the next sample
+        ct_image = read_ct_image(i)
+
+        # generate limited angle trajectory
+        auto sinoDescriptor = LimitedAngleTrajectoryGenerator::createTrajectory(...)
+
+        # create a projector based on it
+        SiddonsMethod projector(ct_image.getDataDescriptor(), *sinoDescriptor)
+
+        # simulate its sinogram
+        auto sinogram = projector.apply(ct_image)
+
+        # setup reconstruction problem
+        WLSProblem wlsProblem(projector, sinogram)
+
+        # solve the reconstruction problem
+        # TODO consider a faster solver
+        CG cgSolver(wlsProblem)
+        # TODO how many iterations to use?
+        auto cgReconstruction = cgSolver.solve(100)
+
+        # write the limited angle reconstruction out
+        EDF::write(cgReconstruction, dir + ct_image.getName() + "_cg_recon_" + i + ".edf")
+
+
+def generate_sparse_ct_data(dir):
+    """
+    Similar to `generate_limited_angle_ct_data` but with CircleTrajectoryGenerator instead of
+    LimitedAngleTrajectoryGenerator
+    """
     pass
 
 
-# TODO ideal plan
-#  1.apply a limited-angle trajectory generator to the data
-#  2.apply the shearlet transform to these altered data
-#  3.apply the custom ADMM to promote edges (L1 reg. promotes sparsity)
-#  4.train the PhantomNet model with this as X and the initial data as Y
+def solve_limited_angle_and_sparse_ct():
+    """
+    Elaborating here a plan for tackling limited angle/sparse CT
+    """
 
-# TODO use only square images for now
-# TODO not entirely sure how important applying ADMM is here
-# TODO note that proper training appears to require A LOT of resources
+    # read the original training data
+    origs_dir = 'orig'
+    data = read(origs_dir)
+
+    # read the original test data
+    tests_dir = 'tests'
+    test_data = read(tests_dir)
+
+    # generate limited angle/sparse ct scans based on existing ones
+    recons_dir = 'recons'
+    generate_limited_angle_ct_data(recons_dir)
+    # or generate_sparse_ct_data(...), or even both and mix them up
+
+    # get the limited angle/sparse images
+    recons_data = read(recons_dir)
+
+    # apply our custom ADMM to promote edges in the data
+    # TODO we don't have this for now, but I'd still like to try this without it, we should still get interpretable
+    #  results
+    admm_recons_data = SHADMM(recons_data)
+
+    # apply the shearlet transform to the output of ADMM
+    shearleted_data = [applyShearletTransform(datum) for datum in admm_recons_data]
+
+    # train the PhantomNet
+    model = PhantomNet(...)
+    model.train(shearleted_data, data)
+    learned_coefficients = model.predict(test_data)
+
+    # last step of the LTI paper, combine visible and invisible coefficients
+    solved_data = applyInverseShearletTransform(learned_coefficients + shearleted_data)
+
+    # compare a sample with the output of the model
+    _plot_sample(test_data[0], 'sample data')
+    _plot_sample(solved_data[0], 'model recon')
 
 
-# altered_data = LimitedAngleTrajectoryGenerator(data)
-
-# shearleted_data = SH(data)
-
-# sparsified_data = SHADMM(...)
-
-# num_of_subbands = ...
-
-# model = PhantomNet(num_of_subbands=num_of_subbands)
-
-# model.train()
-
-# torch.save(model.state_dict(), './phantom_net_model.pth')
+def _plot_sample(img, title):
+    """
+    Helper function for plotting gray images with a title
+    """
+    plt.imshow(img, cmap='gray')
+    plt.title(title)
+    plt.colorbar()
+    plt.show()
 
 
-shearlet_learning_demo()
+"""
+Additional notes
+    1.use only square images for now (the LTI paper only considers square images), perhaps later we can use non-squared
+        images
+    2.not entirely sure how important applying ADMM is here, I assume it should aid the DL model to learn better, but
+        still worth trying without it
+    3.note that proper training appears to require A LOT of resources, overfit on a handful of samples locally, then
+        move to the CIIP machines
+"""
