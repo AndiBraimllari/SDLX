807040804babe097bf7d2d663e7fd5a8ce880da7
andibraimllari <andibraimllari@gmail.com>
2021-08-22 12:59:29 +0200

    add Python script for reconstructing .npy images

---
+++ processing_scripts/gen_sparse_npy_images.py
@@ -0,0 +1,52 @@
+import numpy as np
+import os
+from tqdm import tqdm
+
+import pyelsa as elsa
+
+
+def generate_sparse_ct_images(src_dir, out_dir, num_angles=50, limit=None):
+    """
+    Generate sparsely sampled images from one directory to the other, through elsa. These images are contained in NumPy
+    files.
+    """
+    if not os.path.exists(src_dir):
+        raise ValueError('The provided src_dir directory does not exist')
+
+    if not os.path.exists(out_dir):
+        os.mkdir(out_dir)
+
+    paths = os.listdir(src_dir)
+
+    if limit is not None:
+        paths = paths[:limit]
+
+    for file_name in tqdm(paths):
+        image = elsa.DataContainer(np.load(src_dir + '/' + file_name))
+        size = np.array([image.getDataDescriptor().getNumberOfCoefficientsPerDimension()[0],
+                         image.getDataDescriptor().getNumberOfCoefficientsPerDimension()[1]])
+
+        volume_descriptor = image.getDataDescriptor()
+
+        # generate circular trajectory
+        arc = 360
+        sino_descriptor = elsa.CircleTrajectoryGenerator.createTrajectory(num_angles, volume_descriptor, arc,
+                                                                          size[0] * 100, size[0])
+
+        # setup operator for 2D X-ray transform
+        projector = elsa.SiddonsMethod(volume_descriptor, sino_descriptor)
+
+        # simulate the sinogram
+        sinogram = projector.apply(image)
+
+        # setup reconstruction problem
+        wls_problem = elsa.WLSProblem(projector, sinogram)
+
+        # solve the reconstruction problem
+        cg_solver = elsa.CG(wls_problem)
+
+        no_iterations = 20
+        cg_reconstruction = cg_solver.solve(no_iterations)
+
+        new_image_name = out_dir + '/cg_recon_i_' + str(no_iterations) + '_p_' + str(num_angles) + '_' + file_name
+        np.save(new_image_name, cg_reconstruction)
